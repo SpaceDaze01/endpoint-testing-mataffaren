e74bc88927813ce81d41ba639e1030af5bd4bc11
SpaceDaze01 <luunaastar@gmail.com>
2024-11-16 14:06:55 +0100

    update tests and features

--- tests/features/step-definitions/first_page.js
+++ tests/features/step-definitions/first_page.js
@@ -1,30 +1,37 @@
 
-import { Given, When, Then } from '@cucumber/cucumber';
-import { expect } from 'chai';
-let responseData = null;
+// import { Given, When, Then } from '@cucumber/cucumber';
+// import { expect } from 'chai';
 
-Then('there should be at least {float} main categories', async function (numberOfCategories) {
-  
-  let categoryUrls = [];
-  function getAll(children) {
-    for (let child of children) {
-      categoryUrls.push(child.url);
-      if (child.children) { getAll(child.children); }
-    }
-  }
-  getAll(this.json.children);
+// Given('that I am on the domain {string}', async function(urlPrefix){
+//   this.setUrlPrefix(urlPrefix);
+// });
 
-  expect(categoryUrls.length).to.be.at.least(numberOfCategories);
+// When('I visit the endpoint {string} {string}', async function (method, url){
+//   await this.fetch(url, { method });
+// });
 
-  
-  this.categoryUrlParts = categoryUrls;
-});
+// Then('the status code of the response should be {float}', async function (statusCode){
+//   expect(this.response.status).to.equal(statusCode);
+// });
 
-Then('each main category should have the properties title and url', async function () {
-  for (let category of responseData.data.children) {
-    expect(category.title).to.be.a('string');
-    expect(category.url).to.be.a('string');
-    expect(category.title).to.not.be.empty;
-    expect(category.url).to.not.be.empty;
-  }  
-});
+// Then('the response time should be below {float} milliseconds', async function (ms){
+//   expect(this.responseTime).to.be.below(ms);
+// });
+
+// Then('there should be at least {float} main categories', async function (numberOfCategories){
+//   expect(this.json.children.length).to.be.at.least(numberOfCategories);
+//   // store the category url parts for the next scenario (see usage in feature file)!
+//   this.categoryUrlParts = this.json.children.map(x => x.url);
+// });
+
+// Then('each main category should have the properties title and url', async function(){
+//   // Ensure that each main category has a 'title' and a 'url' and that they're non-empty strings
+//   this.json.children.forEach(category => {
+//     // Assert that each category has a title that is a non-empty string
+//     expect(category.title).to.be.a('string').and.to.not.be.empty;
+
+//     // Assert that each category has a url that is a non-empty string
+//     expect(category.url).to.be.a('string').and.to.not.be.empty;
+//   });
+// });
+
--- tests/features/step-definitions/sverigelottenBla.js
+++ tests/features/step-definitions/sverigelottenBla.js
@@ -2,7 +2,7 @@
 import { Given, When, Then } from '@cucumber/cucumber';
 import { expect } from 'chai';
 
-Then('product should have a valid 'image.url' and 'thumbnail.url'', async function(){
+Then('product should have a valid image.url and thumbnail.url', async function(){
   // TODO: implement step
 });
 
